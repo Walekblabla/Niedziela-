8a44e7ff16351bbfd0384a25a8a9563dfc0e5075
Jan Kowalski <luca.r@wp.pl>
2017-11-19 10:20:50 +0100

    Comicik

---
+++ FormularzKontaktowy/FormularzKontaktowy/Models/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FormularzKontaktowy.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("ContactForm")
+        {
+
+        }
+
+        public DbSet<ContactForm> ContactForms { get; set; }
+    }
+}
